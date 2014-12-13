145260bddd7433c6d432575118558ca3f9ad139b
Neamar <neamar@neamar.fr>
2014-12-13 20:27:19 +0100

    Display date

--- app/src/main/java/bealder/tourmalet/WeatherActivity.java
+++ app/src/main/java/bealder/tourmalet/WeatherActivity.java
@@ -1,6 +1,10 @@
 package bealder.tourmalet;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 
 public class WeatherActivity extends MenuActivity {
@@ -11,5 +15,12 @@ public class WeatherActivity extends MenuActivity {
 				setContentView(R.layout.activity_weather);
 
 				addMenuListeners(R.id.menu_weather);
+
+				TextView dateText = (TextView) findViewById(R.id.date);
+
+				String dateString = DateFormat.getDateInstance(DateFormat.LONG).format(new Date());
+
+				dateString = dateString.substring(0, 1).toUpperCase() + dateString.substring(1).toLowerCase();
+				dateText.setText(dateString);
 		}
 }
