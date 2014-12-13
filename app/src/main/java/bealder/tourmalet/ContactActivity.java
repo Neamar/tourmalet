f8c94b45f8e0135f1bb4b9ef45305c41e80a1549
Neamar <neamar@neamar.fr>
2014-12-13 16:38:52 +0100

    Social links

--- app/src/main/java/bealder/tourmalet/ContactActivity.java
+++ app/src/main/java/bealder/tourmalet/ContactActivity.java
@@ -1,6 +1,10 @@
 package bealder.tourmalet;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 
 public class ContactActivity extends MenuActivity {
@@ -11,5 +15,21 @@ public class ContactActivity extends MenuActivity {
 				setContentView(R.layout.activity_contact);
 
 				addMenuListeners(R.id.menu_contact);
+
+				addSocialLink(R.id.contact_facebook, "https://fr-fr.facebook.com/grandtourmalet");
+				addSocialLink(R.id.contact_twitter, "https://twitter.com/GrandTourmalet");
+				addSocialLink(R.id.contact_instagram, "http://instagram.com/grandtourmalet/");
+		}
+
+		private void addSocialLink(int id, final String url) {
+				ImageView socialButton = (ImageView) findViewById(id);
+				socialButton.setOnClickListener(new View.OnClickListener() {
+						@Override
+						public void onClick(View view) {
+								Intent i = new Intent(Intent.ACTION_VIEW);
+								i.setData(Uri.parse(url));
+								startActivity(i);
+						}
+				});
 		}
 }
